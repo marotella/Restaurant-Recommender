44ad9e74152d12c0abb1c21086531461f2b42dd0
marotella <michelle.a.rotella@gmail.com>
2023-05-08 16:22:48 -0400

    Feature: Added filter for currently open on index.ts

--- index.ts
+++ index.ts
@@ -11,6 +11,9 @@ const filteredRestaurants = restaurants.filter((restaurant) => {
   if (Number(restaurant.priceBracket) > priceBracket) {
     return false;
   }
+  if (hour < Number(restaurant.openHour) ||    hour > Number(restaurant.closeHour)){
+    return false;
+  }
 
   if (restaurant.deliveryTimeMinutes > deliveryTimeMax) {
     return false;
